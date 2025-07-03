cc7e64d4b228b2cfc42765f75321b64a38f75f19
franmarg92 <gabrielleschifranco@gmail.com>
2025-07-03 11:52:00 -0300

    Feat: Midlewares

---
+++ Back/src/midleeware/errorMidleware.js
@@ -0,0 +1,14 @@
+
+const {validationResult } = require ('express-validator')
+
+const handleInputErrors = (req, res, next) =>{
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        res.status(400).json({errors: errors.array()})
+    }
+
+    next()
+
+}
+
+module.exports = {handleInputErrors}
