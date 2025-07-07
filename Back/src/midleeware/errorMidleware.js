e812f3b1f2375a8cbbe3755d861e224efb3bdb72
franmarg92 <gabrielleschifranco@gmail.com>
2025-07-07 15:50:37 -0300

    Feat: middleware

--- Back/src/midleeware/errorMidleware.js
+++ Back/src/midleeware/errorMidleware.js
@@ -1,14 +1,12 @@
+const { validationResult } = require("express-validator");
 
-const {validationResult } = require ('express-validator')
+const handleInputErrors = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+  }
 
-const handleInputErrors = (req, res, next) =>{
-    const errors = validationResult(req)
-    if (!errors.isEmpty()){
-        res.status(400).json({errors: errors.array()})
-    }
+  next();
+};
 
-    next()
-
-}
-
-module.exports = {handleInputErrors}
+module.exports = { handleInputErrors };
