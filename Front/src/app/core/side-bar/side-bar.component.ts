1ccab1073594609095f3f060321dc5edf3bd2aba
franmarg92 <gabrielleschifranco@gmail.com>
2025-07-07 15:48:03 -0300

    Feat: core side bar

---
+++ Front/src/app/core/side-bar/side-bar.component.ts
@@ -0,0 +1,47 @@
+
+import { Component, EventEmitter, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth/auth.service';
+AuthService
+
+@Component({
+  selector: 'app-side-bar',
+  standalone: true,
+  imports: [RouterLink, CommonModule ],
+  templateUrl: './side-bar.component.html',
+  styleUrl: './side-bar.component.css'
+})
+export class SideBarComponent {
+ sidebarItems = [
+    { label: 'Perfil', path: 'profile', roles: ['cliente'] },
+    { label: 'Mis compras', path: 'attendance', roles: ['cliente'] },
+    { label: 'Cargar Productos', path: 'activities', roles: ['admin'] },
+    { label: 'Clientes', path: 'wods', roles: ['admin'] },
+    { label: 'Configuracion', path: 'users', roles: ['admin'] },
+    { label: 'Ventas', path: 'medicalFit', roles: ['admin'] },
+    
+  ];
+
+  filteredSidebarItems: any[] = [];
+  userRole: string = '';
+
+  @Output() closeSidebar = new EventEmitter<void>(); 
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.userRole = this.authService.getUserRole()?? 'user';
+    this.updateSidebarItems();
+  }
+
+  updateSidebarItems() {
+    this.filteredSidebarItems = this.sidebarItems.filter(item =>
+      item.roles.includes(this.userRole)
+    );
+  }
+
+  onItemClick() {
+    this.closeSidebar.emit(); 
+  }
+}
