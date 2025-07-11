f96cd8e9b833a83d5c76b56c35f7e361a4414efb
franmarg92 <gabrielleschifranco@gmail.com>
2025-07-11 12:39:58 -0300

    Feat: product service

---
+++ Front/src/app/services/product/product.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../../models/productModel';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private apiUrl = 'http://localhost:3000/api/product';
+
+  constructor(private http:HttpClient) { }
+
+    editProduct(productData: Product): Observable<Product> {
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  
+      return this.http.put<Product>(
+        `${this.apiUrl}/edit`,
+         productData ,
+        { headers }
+      );
+    }
+
+    createProduct(productData:Product ): Observable<Product>{
+       
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  
+      return this.http.post<Product>(
+        `${this.apiUrl}/create`,
+        { product : productData },
+        { headers }
+      );
+    }
+}
