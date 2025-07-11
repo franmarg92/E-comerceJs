51a548d5897428f0cfeefb6d88454e4b9970794f
franmarg92 <gabrielleschifranco@gmail.com>
2025-07-11 12:52:18 -0300

    Add routers

--- Back/app.js
+++ Back/app.js
@@ -3,8 +3,14 @@ require("dotenv").config();
 const passport = require("passport");
 const cors = require("cors");
 const { connectDB } = require("./src/config");
-const corsConfig = require('./src/config')
-const {registerRouter, loginRouter} = require('./src/routes')
+const corsConfig = require("./src/config");
+const {
+  registerRouter,
+  loginRouter,
+  userRouter,
+  productRouter,
+  categoryRouter,
+} = require("./src/routes");
 
 const app = express();
 const PORT = 3000;
@@ -13,9 +19,11 @@ app.use(express.json());
 app.use(cors(corsConfig));
 app.use(passport.initialize());
 
-app.use('/api/auth', registerRouter)
-app.use('/api/auth', loginRouter)
-
+app.use("/api/auth", registerRouter);
+app.use("/api/auth", loginRouter);
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/category", categoryRouter);
 
 app.listen(PORT, async () => {
   await connectDB.initDB();
