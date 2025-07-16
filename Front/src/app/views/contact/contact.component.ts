66842cd1c2b485421a0b694d9430627c18d79e0a
franmarg92 <gabrielleschifranco@gmail.com>
2025-07-16 16:33:53 -0300

    Feat: contact component

--- Front/src/app/views/contact/contact.component.ts
+++ Front/src/app/views/contact/contact.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  standalone: true,
-  imports: [],
+  standalone:  true,
+  imports: [ReactiveFormsModule, ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: [''],
+      email: [''],
+      message: ['']
+    });
+  }
 }
