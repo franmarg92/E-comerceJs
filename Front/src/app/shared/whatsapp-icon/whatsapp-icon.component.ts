65bb37d21093cda96909c1e198149568a165c443
franmarg92 <gabrielleschifranco@gmail.com>
2025-07-16 16:30:35 -0300

    Add shared icon wsp

---
+++ Front/src/app/shared/whatsapp-icon/whatsapp-icon.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-whatsapp-icon',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './whatsapp-icon.component.html',
+  styleUrl: './whatsapp-icon.component.css'
+})
+export class WhatsappIconComponent {
+
+}
