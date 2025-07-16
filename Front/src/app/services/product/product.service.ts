22498297668ca05e8ffe7fd0f5fda667cb85fc17
franmarg92 <gabrielleschifranco@gmail.com>
2025-07-16 16:33:34 -0300

    Add back apis conection product

--- Front/src/app/services/product/product.service.ts
+++ Front/src/app/services/product/product.service.ts
@@ -3,32 +3,36 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../../models/productModel';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
   private apiUrl = 'http://localhost:3000/api/product';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    editProduct(productData: Product): Observable<Product> {
-      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-  
-      return this.http.put<Product>(
-        `${this.apiUrl}/edit`,
-         productData ,
-        { headers }
-      );
-    }
+  editProduct(productData: Product): Observable<Product> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-    createProduct(productData:Product ): Observable<Product>{
-       
-      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-  
-      return this.http.post<Product>(
-        `${this.apiUrl}/create`,
-        { product : productData },
-        { headers }
-      );
-    }
+    return this.http.put<Product>(`${this.apiUrl}/edit`, productData, {
+      headers,
+    });
+  }
+
+  createProduct(productData: Product): Observable<Product> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    return this.http.post<Product>(
+      `${this.apiUrl}/create`,
+      { product: productData },
+      { headers }
+    );
+  }
+
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.apiUrl}/`);
+  }
+
+  getProductById(id: string): Observable<{ product: Product }> {
+  return this.http.get<{ product: Product }>(`${this.apiUrl}/${id}`);
+}
 }
