69f71af2046cb8aceb5ccd88d19e32f436c4e05f
franmarg92 <gabrielleschifranco@gmail.com>
2025-08-01 11:17:29 -0300

    Ruteando imagenes

--- Back/app.js
+++ Back/app.js
@@ -27,8 +27,11 @@ app.use(express.json());
 app.use(cors(corsConfig));
 app.use(passport.initialize());
 
+// Ruta absoluta a la carpeta de imágenes
+const imagePath = path.join(__dirname, 'storage', 'imgs');
+
 // Servir archivos estáticos
-app.use('/imgs', express.static('./src/storage/imgs'));
+app.use('/imgs', express.static(imagePath));
  
 // Logging básico
 app.use((req, res, next) => {
