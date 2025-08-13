926c6a9a57c6400190d7bc7e5d0487d03a34f16d
franmarg92 <gabrielleschifranco@gmail.com>
2025-08-13 19:12:50 -0300

    probando

--- Back/src/routes/mercadoPagoRoutes.js
+++ Back/src/routes/mercadoPagoRoutes.js
@@ -4,7 +4,7 @@ const { mercadoPagoController } = require("../controllers");
 
 router.post("/create-preference", mercadoPagoController.createPreferenceController);
 router.post("/webhook", mercadoPagoController.handleWebhook);
-router.post("/hook", req, res => {
+router.post("/hook", (req, res) => {
   console.log("Webhook recibido:", req.body);
   res.status(200).send("Webhook recibido");});
 module.exports = router;
