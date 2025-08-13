9ecf68bf1f8e7f983676aca87079400bc3ff74fa
franmarg92 <gabrielleschifranco@gmail.com>
2025-08-13 19:04:47 -0300

    probando logs

--- Back/src/controllers/mercadoPagoController.js
+++ Back/src/controllers/mercadoPagoController.js
@@ -22,20 +22,7 @@ const createPreferenceController = async (req, res) => {
 };
 
 const handleWebhook = async (req, res) => {
- try {
-    const { type, data, action } = req.body;
-
-    if (type !== "payment" || !data?.id || action !== "payment.updated") {
-      console.warn("Webhook ignorado por formato inválido");
-      return res.status(400).send("Payload inválido");
-    }
-
-    await mercadoPagoService.processWebhook(req.body);
-    return res.status(200).send("Webhook procesado");
-  } catch (error) {
-    console.error("Error procesando webhook:", error);
-    return res.status(500).send("Error interno");
-  }
+consle.log(req.body);
 };
 
 
